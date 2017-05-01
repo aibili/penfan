812e46f2f2c9fc67a3fa58d184aee705a4402d07
aibili <442757052@163.com>
2017-05-01 14:26:00 +0800

    Add files via upload

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,66 @@
 
+$(function(){
+	
+	$('[data-toggle="tooltip"]').tooltip();
+	function resize(){
+		
+		var windowwidth = $(window).width();
+		
+		$("#main_ad>.carousel-inner>.item").each(function(i,item){
+			
+			var $item = $(item);
+			
+			if(windowwidth<768){
+				
+				var $imag = $item.data("image-sm");
+				//$item.css("backgroundImage","url("+$imag+")");
+				$item.css("backgroundImage","url()");
+				$item.html("<img src="+$imag+">");
+			}
+			else{
+				
+				$item.html("");
+				var $imag = $item.data("image-lg");
+				$item.css("backgroundImage","url("+$imag+")");
+		
+				
+			}
+			});	
+	};
+	
+	
+	$(window).on("resize",resize).trigger("resize");
+	
+	
+	var $lis = $(".nav-tabs").children();
+	
+	var width =0;
+	
+     $lis.each(function(index,li){
+		 
+		
+		 width = width+li.clientWidth;
+		 });
+		 
+	
+	$(window).on("resize",function(){
+		
+		if($(window).width()<768){
+			
+			$(".nav-tabs").css("width",width+20);
+		
+	}else{
+		
+		$(".nav-tabs").css("width","100%");
+	}
+		
+		
+		});
+	
+	
+
+	
+	
+	
+	});
+		
